685686aee103e19c99187a98f5ca14d00fbdc675
abdel trs <abdeltrs@hotmail.com>
2020-12-09 23:16:22 +0100

    added friends system

---
+++ friends.php
@@ -0,0 +1,82 @@
+<?php 
+    session_start();
+
+    require 'include/class.friends.php';
+    include_once 'include/class.user.php';
+    include_once 'include/db_config.php';
+
+    $user = new User();
+
+    $uid = $_SESSION['uid'];
+
+    if (!$user->get_session()){
+       header("location:login.php");
+    }
+
+    // Deconnexion
+    if (isset($_GET['q'])){
+        $user->user_logout();
+        header("location:login.php");
+    }
+
+    $db = new DB_con(); 
+    $db = $db->ret_obj();
+
+    $query = $db->prepare("SELECT * FROM users");
+    $query->execute();
+
+    if($query){
+        while($fetch = $query->fetch()){
+            $id = $fetch['uid'];
+            $username = $fetch['uname'];
+        }
+    }
+
+?>
+  <!DOCTYPE html>
+  <html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Projet POO Ajax SQL - Friends</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  </head>
+
+<!-- Base stylisée par bootstrap -->
+
+  <body>
+    <div id="container" class="container">
+      <div id="header">
+        <a href="index.php?q=logout">Se déconnecter</a>
+      </div>
+      <div id="main-body">
+        <br/>
+        <br/>
+        <h1>
+          Liste d'amis
+    	</h1>
+        <br/>
+        <h3><?php $user->get_fullname($uid); ?></h3>
+        <br/>
+        <div>
+        <?php 
+            if($id = $_SESSION['uid']){
+                if(Friends::renderFriendShip($_SESSION['uid'], $id, 'requestPending') == 1) {
+        ?>
+            <button class="btn" disabled>Request Pending</button>
+        <?php
+
+                }
+            }else {
+
+            }
+        ?>
+        </div>
+      </div>
+      <div id="footer">
+      Projet réalisé par Abdel, Yann, Adrien, et Romain - IIM 2020
+      </div>
+    </div>
+  </body>
+
+  </html>
---
+++ include/class.friends.php
@@ -0,0 +1,26 @@
+<?php
+    include "class.user.php";
+    
+    class Friends {
+        static public function renderFriendShip($user_one, $user_two, $type){
+            if(!empty($user_one) && !empty($user_two)){
+                global $db;
+
+                switch($type){
+                    case 'requestPending':
+                        $query = $db->prepare("SELECT * FROM friends WHERE user_one='".$user_one."' AND user_two='".$user_two."' AND friendship_officiel='0' OR user_one='".$user_two."' AND user_two='".$user_one."' AND friendship_officiel='0'");
+                        $query->execute();
+
+                        return $query->rowCount();
+                        break;
+
+                    case 'alreadyFriends': 
+                        $query = $db->prepare("SELECT * FROM friends WHERE user_one='".$user_one."' AND user_two='".$user_two."' AND friendship_officiel='1' OR user_one='".$user_two."' AND user_two='".$user_one."' AND friendship_officiel='1'");
+                        $query->execute();
+
+                        return $query->rowCount();
+                        break;
+                }
+            }
+        }
+    }
